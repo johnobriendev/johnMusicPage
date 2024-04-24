af70b59e572453cb695670422830ec534888f280
John O'Brien <johnobrien.dev@gmail.com>
2024-04-24 08:12:47 -0400

    change style in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ export default function Navbar(){
    const [menuOpen, setMenuOpen] = useState(false);
    
     return(
-        <nav className="flex flex-col items-start gap-5 md:flex-row md:justify-between md:items-center bg-slate-900 text-white p-5 sticky top-0">
-            <Link to='/' className="text-3xl hover:bg-slate-700 px-3 py-1 rounded" onClick={() => setMenuOpen(false)}>John O'Brien</Link>
+        <nav className="flex flex-col items-start gap-5 font-extralight md:flex-row md:justify-between md:items-center bg-slate-900 text-white px-10 py-5 sticky top-0">
+            <Link to='/' className="text-3xl hover:bg-slate-700 px-3 py-1 rounded md:text-5xl" onClick={() => setMenuOpen(false)}>John O'Brien</Link>
             
                 <div className="text-2xl md:hidden cursor-pointer absolute top-5 right-5" onClick={() => setMenuOpen(!menuOpen)}>
                     <RxHamburgerMenu />
                 </div>
-                <ul className={menuOpen ? "flex flex-col gap-5 w-screen h-screen " : "hidden md:flex"}>
+                <ul className={menuOpen ? "flex flex-col gap-5 w-screen h-screen " : "hidden md:flex md:text-xl"}>
                     <li><NavLink to='/about' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(false)}>About</NavLink></li>
                     <li><NavLink to='/music' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(false)}>Music</NavLink></li>
                     <li><NavLink to='contact' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(false)}>Contact</NavLink></li>
