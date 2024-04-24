8813ceb5588f82bd1d4e26ad5975ff9babeec889
John O'Brien <johnobrien.dev@gmail.com>
2024-04-24 08:31:33 -0400

    add image to homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import { FaSpotify } from "react-icons/fa6";
 
 export default function Footer() {
     return(
-        <div className=" flex items-center justify-center gap-20 w-full h-[100px] bg-slate-800 absolute bottom-0 text-white text-3xl">
+        <div className=" flex items-center justify-center gap-20 w-full h-[100px] bg-slate-900 absolute bottom-0 text-white text-3xl">
             <FaInstagram className="hover:scale-125 duration-150"/>
             <FaYoutube className="hover:scale-125 duration-150" />
             <FaSpotify className="hover:scale-125 duration-150" />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 export default function Home(){
     return(
         <div className="w-full h-full flex flex-col gap-10 items-center py-10 px-2">
+            <img src="john-home.jpg" alt="" className=""/>
             <h1 className="text-xl">Guitarist, Composer, Educator</h1>
             <div className="h-[400px] min-w-[300px] max-w-[400px] bg-pink-300">This is a fake image</div>
             <p className="p-2">John is a guitar player from Bethlehem, Pennsylvania.</p>
