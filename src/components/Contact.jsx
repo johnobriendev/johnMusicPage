e0b70cf4bc868ea820ab2ef6b1c48f818d7c94c2
John O'Brien <johnobrien.dev@gmail.com>
2024-04-29 09:06:41 -0400

    change font color on contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,10 @@ export default function Contact() {
     return ( 
         <div className="py-10 px-4 mx-auto max-w-screen-md h-full"> 
             <h2 className="mb-4 text-4xl font-extrabold  
-                           text-center text-gray-900"> 
+                           text-center"> 
                 Contact 
             </h2> 
-            <p className="mb-4 font-light text-left  
-                          text-gray-500 sm:text-xl"> 
+            <p className="mb-4 font-light text-left sm:text-xl"> 
                 If you have any questions please use the contact form below.
             </p> 
             <form action="#"> 
@@ -14,12 +13,12 @@ export default function Contact() {
                     <div className="w-1/2 pr-2 "> 
                         <label for="firstName" 
                                className="block my-2 text-left  
-                                          text-sm font-medium text-gray-900"> 
+                                          text-sm font-medium"> 
                             First Name 
                         </label> 
                         <input type="text" 
                                className="shadow-sm bg-gray-50 border 
-                                          border-gray-300 text-gray-900  
+                                          border-gray-30m  
                                           text-sm rounded-lg block w-full p-2.5" 
                                placeholder="Enter First Name"
                                required/> 
@@ -27,12 +26,12 @@ export default function Contact() {
                     <div className="w-1/2 pl-2"> 
                         <label for="lastName" 
                                className="block my-2 text-left text-sm  
-                                          font-medium text-gray-900"> 
+                                          font-medium"> 
                             Last Name 
                         </label> 
                         <input type="text" 
                                className="shadow-sm bg-gray-50 border  
-                                          border-gray-300 text-gray-900  
+                                          border-gray-300   
                                           text-sm rounded-lg block w-full p-2.5"
                                placeholder="Enter Last Name"/> 
                     </div> 
@@ -40,12 +39,12 @@ export default function Contact() {
                 <div> 
                     <label for="email" 
                            className="block my-2 text-left text-sm  
-                                      font-medium text-gray-900"> 
+                                      font-medium "> 
                         Your email 
                     </label> 
                     <input type="email" 
                            className="shadow-sm bg-gray-50 border  
-                                      border-gray-300 text-gray-900  
+                                      border-gray-300   
                                       text-sm rounded-lg block w-full p-2.5" 
                            placeholder="Enter your email " 
                            required /> 
@@ -53,12 +52,12 @@ export default function Contact() {
                 <div> 
                     <label for="subject" 
                            className="block my-2 text-left  
-                                      text-sm font-medium text-gray-900"> 
+                                      text-sm font-medium"> 
                         Subject 
                     </label> 
                     <input type="text" 
                            className="block p-3 w-full text-sm  
-                                      text-gray-900 bg-gray-50 rounded-lg  
+                                      bg-gray-50 rounded-lg  
                                       border border-gray-300 shadow-sm "
                            placeholder="Add a subject line" 
                            required /> 
@@ -66,12 +65,12 @@ export default function Contact() {
                 <div > 
                     <label for="message" 
                            className="block my-2 text-left  
-                                      text-sm font-medium text-gray-900 "> 
+                                      text-sm font-medium "> 
                         Your message 
                     </label> 
                     <textarea rows="6" 
                               className="block p-2.5 w-full text-sm  
-                                         text-gray-900 bg-gray-50 rounded-lg  
+                                         bg-gray-50 rounded-lg  
                                          shadow-sm border border-gray-300 " 
                               placeholder="Write your message here"/> 
                 </div> 
